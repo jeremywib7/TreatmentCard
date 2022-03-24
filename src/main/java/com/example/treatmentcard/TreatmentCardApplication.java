e9bf41c5c875e979bafd5e9502578430958e6c40
Jeremy Yonathan <jeremywib7@gmail.com>
2022-03-24 18:01:04 +0700

    Initial commit

---
+++ src/main/java/com/example/treatmentcard/TreatmentCardApplication.java
@@ -0,0 +1,13 @@
+package com.example.treatmentcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreatmentCardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TreatmentCardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/treatmentcard/TreatmentCardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.treatmentcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreatmentCardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
